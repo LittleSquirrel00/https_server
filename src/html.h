b40639367999893c93b9c7b555322e0e6e4671ce
F.Zhang <littlesquirrel00@gmail.com>
2019-12-06 10:38:35 +0800

    add comments

--- src/html.h
+++ src/html.h
@@ -1,8 +1,12 @@
 #ifndef HTML_H_DEFINED_
 #define HTML_H_DEFINED_
 
+/** 读取404的html页面
+ */
 char *HTTP_NotFound();
 
+/** 读取403的html页面
+ */
 char *HTTP_Forbidden();
 
 #endif
--- src/parser.h
+++ src/parser.h
@@ -42,11 +42,7 @@ typedef struct {
     Action act;
 } Ack_Data;
 
-int on_message_begin(http_parser *parser);
-
 int on_url(http_parser *parser, const char *at, size_t length);
-
-int on_status(http_parser *parser, const char *at, size_t length);
 
 int on_header_field(http_parser *parser, const char *at, size_t length);
 
@@ -55,12 +51,6 @@ int on_header_value(http_parser *parser, const char *at, size_t length);
 int on_headers_complete(http_parser *parser);
 
 int on_body(http_parser *parser, const char *at, size_t length);
-
-int on_message_complete(http_parser *parser);
-
-int on_chunk_header(http_parser *parser);
-
-int on_chunk_complete(http_parser *parser);
 
 /***************************************************/
 
@@ -76,16 +66,30 @@ Ack_Data *data_init(Ack_Data *data);
 
 void data_free(Ack_Data *data);
 
+/** 解析路径，判断执行操作
+ *  实现：
+ *      判断路径是否存在，是否具有读写权限
+ *      判断分块传输，文件大小
+ */
 static void path_parse(http_parser *parser);
 
+/** 读取目录，生成html页面
+ */
 static void read_directory(http_parser *parser);
 
 static void read_file(http_parser *parser);
 
+
+/** 上传小文件
+ *  实现：
+ *      根据boundary确定传输的数据
+ */
 static void post_file(http_parser *parser);
 
 /***************************************************/
 
+/** 生成回答报文的http header
+ */
 static void mkheader(http_parser *parser);
 
 static void header_set_server(char *header);
@@ -94,18 +98,33 @@ static void header_set_date(char *header);
 
 static void header_set_content_type(char *header, http_parser *parser);
 
+/** 设置回答报文的body长度
+ *  实现：
+ *      分块传输不需要设置
+ *      下载大文件时返回文件大小
+ *      其他返回body大小
+ */
 static void header_set_content_length(char *header, http_parser *parser);
 
 static void header_set_chunked(char *header, http_parser* parser);
 
+/** 确定是否是持久连接
+ */
 static void header_set_connection(char *header, http_parser *parser);
 
 /***************************************************/
 
+/** 确认是否使用分块传输
+ *  实现： 
+ *      是否是dynamic/目录下文件
+ *      其他情况待定
+ */
 static int ischunk(char *path);
 
 int http_body_final(http_parser *parser);
 
+/** 确定是否需要额外io线程(下载/分块)
+ */
 int need_thread(http_parser *parser);
 
 #endif
